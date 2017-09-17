a4cd3a22178ccb0684d5f29a34ef50cce1e288f0
Jorge Barroso <jorge_barroso_11@hotmail.com>
2017-09-17 23:25:35 +0100

    Checkstyle fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   * 
+   * @param someone
+   * @return Hello string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
